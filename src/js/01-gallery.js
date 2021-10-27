08b17b3ac110eeaa5ed800c1c77205a2d5512261
Maksym Lvov <leva880@ukr.net>
2021-10-27 17:28:13 +0300

    updates

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,12 +2,16 @@ import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
+
+
 let gallery = document.querySelector('.gallery');
 
 const markup = galleryItems.map(item => `
-<a class="gallery__link" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>
+ <div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"/>
+    </a>
+  </div>
 `).join('');
 
 gallery.innerHTML = markup;
