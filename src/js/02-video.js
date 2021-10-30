c51c4c313e736630da191e77c4a878ba6aeefe56
Maksym Lvov <leva880@ukr.net>
2021-10-30 15:16:34 +0300

    fixed code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,29 @@
-
 import Player from '@vimeo/player';
 
+var iframe = document.querySelector('iframe');
+var player = new Vimeo.Player(iframe);
+const currentTimeValue = JSON.parse(localStorage.getItem('videoplayer-current-time'));
 
+function videoCurrentTime(event) {
+  const settings = {
+    seconds: `${event.seconds}`,
+    percent: `${event.percent}`,
+    duration: `${event.duration}`,
+  };
+
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(settings));
+}
+
+if (currentTimeValue) {
+  player.setCurrentTime(currentTimeValue.seconds).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+}
+
+player.on('timeupdate', videoCurrentTime);
