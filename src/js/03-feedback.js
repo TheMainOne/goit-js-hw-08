22decdc55de6855ba0b99986894c0853c543c172
Maksym Lvov <leva880@ukr.net>
2021-10-30 16:53:45 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+console.log(form.elements);
+
+form.addEventListener('input', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const DataForStorage = {
+        email: "",
+        text: '',
+    }
+    // formData.forEach((email) => console.log(email))
+    console.log(event.elements)
+});
